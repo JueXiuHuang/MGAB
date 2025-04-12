cd27442f115dc0a305aa6daa488d4f255548cda3
JueXiuHuang <joshua87719@gmail.com>
2025-04-12 23:50:05 +0800

    refactor screen

--- screen.py
+++ screen.py
@@ -1,4 +1,5 @@
 import traceback
+from abc import ABC, abstractmethod
 from ctypes import windll
 
 import cv2
@@ -7,23 +8,42 @@ import win32ui
 from PIL import Image
 
 from adb import ADB
-from mode import ControlMode
 
 
-class Screen:
-  def __init__(self, _mode: ControlMode, _hwnd: int | None = None) -> None:
-    """Init screen object with mode & window number."""
-    self.mode = _mode
-    self.hwnd = _hwnd
-    if self.mode == ControlMode.WIN32API:
-      if self.hwnd is None:
-        raise Exception("Need hwnd in WIN32API mode")
-    elif self.mode == ControlMode.ADB:
-      pass
+class Screen(ABC):
+  """Define screen related operation."""
+
+  @abstractmethod
+  def get_screenshot(self, zoom_ratio: float) -> tuple[bool, Image.Image]:
+    """Get screenshot."""
+
+
+class ADBScreen(Screen):
+  """Screen related operation with ADB."""
 
   def adb() -> ADB:
     """Dynamically get ADB instance."""
     return ADB("")
+
+  def get_screenshot(self, zoom_ratio: float) -> tuple[bool, Image.Image]:
+    """Get screenshot."""
+    try:
+      img = self.adb.screenshot()
+      height, width = img.shape[:2]
+      img = cv2.resize(img, (int(width * zoom_ratio), int(height * zoom_ratio)))
+    except:
+      print(traceback.format_exc())
+      return (False, None)
+    return (True, img)
+
+
+class WIN32Screen(Screen):
+  """Screen related operation with win32 api."""
+
+  def __init__(self, _hwnd: int) -> None:  # noqa: D107
+    self.hwnd = _hwnd
+    if self.hwnd is None:
+      raise Exception("Need hwnd in WIN32API mode")
 
   def get_window_size_info(self) -> tuple[int, int, int, int]:
     """Get window size info."""
@@ -41,44 +61,34 @@ class Screen:
 
   def get_screenshot(self, zoom_ratio: float) -> tuple[bool, Image.Image]:
     """Get screenshot."""
-    if self.mode == ControlMode.WIN32API:
-      _, _, w, h = self.getWindowSizeInfo()
-      # windows zoom setting
-      w = int(w * zoom_ratio)
-      h = int(h * zoom_ratio)
+    _, _, w, h = self.getWindowSizeInfo()
+    # windows zoom setting
+    w = int(w * zoom_ratio)
+    h = int(h * zoom_ratio)
 
-      hwnddc = win32gui.GetWindowDC(self.hwnd)
-      mfcdc = win32ui.CreateDCFromHandle(hwnddc)
-      savedc = mfcdc.CreateCompatibleDC()
+    hwnddc = win32gui.GetWindowDC(self.hwnd)
+    mfcdc = win32ui.CreateDCFromHandle(hwnddc)
+    savedc = mfcdc.CreateCompatibleDC()
 
-      bmp = win32ui.CreateBitmap()
-      bmp.CreateCompatibleBitmap(mfcdc, w, h)
+    bmp = win32ui.CreateBitmap()
+    bmp.CreateCompatibleBitmap(mfcdc, w, h)
 
-      savedc.SelectObject(bmp)
+    savedc.SelectObject(bmp)
 
-      result = windll.user32.PrintWindow(self.hwnd, savedc.GetSafeHdc(), 3)
-      print(result)
+    result = windll.user32.PrintWindow(self.hwnd, savedc.GetSafeHdc(), 3)
+    print(result)
 
-      bmpinfo = bmp.GetInfo()
-      bmpstr = bmp.GetBitmapBits(True)
+    bmpinfo = bmp.GetInfo()
+    bmpstr = bmp.GetBitmapBits(True)
 
-      if bmpinfo["bmWidth"] != 1 or bmpinfo["bmHeight"] != 1:
-        im = Image.frombuffer("RGB", (bmpinfo["bmWidth"], bmpinfo["bmHeight"]), bmpstr, "raw", "BGRX", 0, 1)
-      else:
-        im = None
+    if bmpinfo["bmWidth"] != 1 or bmpinfo["bmHeight"] != 1:
+      im = Image.frombuffer("RGB", (bmpinfo["bmWidth"], bmpinfo["bmHeight"]), bmpstr, "raw", "BGRX", 0, 1)
+    else:
+      im = None
 
-      win32gui.DeleteObject(bmp.GetHandle())
-      savedc.DeleteDC()
-      mfcdc.DeleteDC()
-      win32gui.ReleaseDC(self.hwnd, hwnddc)
+    win32gui.DeleteObject(bmp.GetHandle())
+    savedc.DeleteDC()
+    mfcdc.DeleteDC()
+    win32gui.ReleaseDC(self.hwnd, hwnddc)
 
-      return ((im is not None), im)
-
-    try:
-      img = self.adb.screenshot()
-      height, width = img.shape[:2]
-      img = cv2.resize(img, (int(width * zoom_ratio), int(height * zoom_ratio)))
-    except:
-      print(traceback.format_exc())
-      return (False, None)
-    return (True, img)
+    return ((im is not None), im)
